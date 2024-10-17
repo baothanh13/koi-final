2046c128f197cc5c1b58e1f95efdda4a61a12142
order <nguyenngocquy612@gmail.com>
2024-10-17 20:56:11 +0700

    Default Layout

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+// Pages
+import Home from "../pages/client/Home";
+
+// Public routes
+const publicRoutes = [{ path: "/", component: Home }];
+
+const privateRoutes = [];
+export { publicRoutes, privateRoutes };
